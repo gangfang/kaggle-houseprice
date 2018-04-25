7843a7ab1f60289e87a91c30848358a39e3e6442
GANG_FANG <gfang1212@gmail.com>
2018-04-25 10:52:37 +0800

    Did first round of feature engineering
    got a RMSE of around 1.57.

--- solver.py
+++ solver.py
@@ -4,7 +4,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.linear_model import LinearRegression
-from sklearn.cross_validation import train_test_split
+# import xgboost as xgb
+from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
 
 np.set_printoptions(threshold=sys.maxsize)
@@ -22,7 +23,7 @@ def main():
   split_train_test_sets_at(trainset_length)
   train_model()
   predict()
-  write_result_csv()
+  # write_result_csv()
 
 
 
@@ -35,16 +36,17 @@ def acquire_data():
 
 
 def understand_data():
-  print(combined_df.head())  
-  print(combined_df.describe())
-  print(combined_df.info())
+  train_df.info()
+  test_df.info()
 
 
 def plot_top_corr_heatmap():
   corr_matrix = train_df.corr(method='pearson')
   cols = corr_matrix.nlargest(10, 'SalePrice').index
   largest_corr_matrix = np.corrcoef(train_df[cols].values.T)
-  sns.heatmap(largest_corr_matrix, cbar=True, annot=True, square=True, fmt='.2f', annot_kws={'size': 10}, yticklabels=cols.values, xticklabels=cols.values)
+  sns.heatmap(largest_corr_matrix, cbar=True, annot=True, 
+              square=True, fmt='.2f', annot_kws={'size': 10}, 
+              yticklabels=cols.values, xticklabels=cols.values)
   plt.show()
   
 
@@ -56,8 +58,9 @@ def prepare_data():
   combined_df = grasp_features_of_top_corr(combined_df)
   # combined_df = drop_useless_features_from(combined_df)
   handle_missing_data_for(combined_df)
+  combined_df = transform_features_for(combined_df)
   combined_df = create_new_features_for(combined_df)
-  # combined_df = one_hot_encode_categorical_features_of(combined_df)
+  combined_df = one_hot_encode_categorical_features_of(combined_df)
   trainset_length = get_trainset_length(train_df)
   
 
@@ -73,36 +76,61 @@ def concat_train_test_data(train_df, test_df):
 
 
 def grasp_features_of_top_corr(dataset_df):
-  selected_features = ['OverallQual', 'GrLivArea', 'GarageCars', 'GarageArea', '1stFlrSF', 'FullBath', 'TotRmsAbvGrd', 'YearBuilt']
+  selected_features = ['OverallQual', 'GrLivArea', 'GarageCars', \
+                       'GarageArea', 'TotalBsmtSF', 'LotFrontage',\
+                       'FullBath', 'TotRmsAbvGrd', 'YearBuilt', \
+                       'YearRemodAdd', 'ExterQual', 'BsmtQual', \
+                       'Neighborhood', '1stFlrSF']
   return dataset_df[selected_features]
     
 
 def drop_useless_features_from(dataset_df):
-  return dataset_df.drop(['GarageYrBlt', 'Id'], axis=1)
+  return dataset_df.drop([], axis=1)
 
 
 def handle_missing_data_for(dataset_df):
-  # dataset_df['LotFrontage'].fillna(dataset_df['LotFrontage'].dropna().median(), inplace=True)
+  dataset_df['LotFrontage'].fillna(dataset_df['LotFrontage'].dropna().median(), inplace=True)
   # dataset_df['MasVnrArea'].fillna(dataset_df['MasVnrArea'].dropna().median(), inplace=True)
   # dataset_df['MasVnrType'].fillna('None', inplace=True)
+  dataset_df['BsmtQual'].fillna('None', inplace=True)
   # dataset_df['Electrical'].fillna('SBrkr', inplace=True)
   # dataset_df['BsmtFinSF1'].fillna(dataset_df['BsmtFinSF1'].dropna().median(), inplace=True)
   # dataset_df['BsmtFinSF2'].fillna(dataset_df['BsmtFinSF2'].dropna().median(), inplace=True)
   # dataset_df['BsmtUnfSF'].fillna(dataset_df['BsmtUnfSF'].dropna().median(), inplace=True)
-  # dataset_df['TotalBsmtSF'].fillna(dataset_df['TotalBsmtSF'].dropna().median(), inplace=True)
+  dataset_df['TotalBsmtSF'].fillna(dataset_df['TotalBsmtSF'].dropna().median(), inplace=True)
   # dataset_df['BsmtFullBath'].fillna(0, inplace=True)
   # dataset_df['BsmtHalfBath'].fillna(0, inplace=True)
   dataset_df['GarageCars'].fillna(2, inplace=True)
   dataset_df['GarageArea'].fillna(dataset_df['GarageArea'].dropna().median(), inplace=True)
 
 
+def transform_features_for(dataset_df):
+  QUALITY_MAP = {"None":0, "Fa":1, "TA":2, "Gd":3, "Ex":4}
+  dataset_df['ExterQual'] = dataset_df['ExterQual'].map(QUALITY_MAP)
+  dataset_df['BsmtQual'] = dataset_df['BsmtQual'].map(QUALITY_MAP)
+  return dataset_df
+
+
 def create_new_features_for(dataset_df):
   dataset_df['OverallQual_quad'] = dataset_df['OverallQual'] ** 2
+  dataset_df["OverallQual_cub"] = dataset_df["OverallQual"] ** 3
+  dataset_df['GrLivArea_quad'] = dataset_df['GrLivArea'] ** 2
+  dataset_df['GrLivArea_cub'] = dataset_df['GrLivArea'] ** 3
+  dataset_df['GarageCars_quad'] = dataset_df['GarageCars'] ** 2
+  dataset_df['GarageCars_cub'] = dataset_df['GarageCars'] ** 3
+  dataset_df['TotRmsAbvGrd_quad'] = dataset_df['TotRmsAbvGrd'] ** 2
+  dataset_df['BsmtQual_quad'] = dataset_df['BsmtQual'] ** 2
+  dataset_df['BsmtQual_cub'] = dataset_df['BsmtQual'] ** 3
+  dataset_df['LotFrontage_quad'] = dataset_df['LotFrontage'] ** 2
+  dataset_df['LotFrontage_cub'] = dataset_df['LotFrontage'] ** 3
   return dataset_df
 
 
 def one_hot_encode_categorical_features_of(dataset_df):
-  return pd.get_dummies(dataset_df, dummy_na=True, drop_first=True)
+  return pd.get_dummies(dataset_df, 
+                        columns=['Neighborhood'], prefix='Neighborhood', 
+                        dummy_na=True, drop_first=True)
+  # return pd.get_dummies(dataset_df, dummy_na=True, drop_first=True)
 
 
 def get_trainset_length(train_df):
@@ -110,13 +138,15 @@ def get_trainset_length(train_df):
 
 
 def split_train_test_sets_at(trainset_length):
-  global X_train, X_pred, y_train
-  X_train = combined_df.iloc[:trainset_length, :]
-  X_pred = combined_df.iloc[trainset_length:, :]
-  y_train = target_col
+  # global X_train, X_pred, y_train
+  # X_train = combined_df.iloc[:trainset_length, :]
+  # X_pred = combined_df.iloc[trainset_length:, :]
+  # y_train = target_col
 
-  # global X_train, X_test, y_train, y_test
-  # X_train, X_test, y_train, y_test = train_test_split(combined_df.iloc[:trainset_length, :], target_col, random_state=3)
+  global X_train, X_test, y_train, y_test
+  X_train, X_test, y_train, y_test = train_test_split(\
+                                        combined_df.iloc[:trainset_length, :],\
+                                        target_col, random_state=2)
 
 
 
@@ -127,17 +157,20 @@ def train_model():
 
 
 def predict():
-  global y_pred
-  y_pred = linear_regression.predict(X_pred)
+  # global y_pred
+  # y_pred = linear_regression.predict(X_pred)
+  # y_pred[y_pred < target_col.min()] = target_col.min()
 
-  # y_pred_on_trainset = linear_regression.predict(X_train)
-  # y_pred = linear_regression.predict(X_test)
-  # y_train_log = np.log(y_train)
-  # y_pred_on_trainset_log = np.log(y_pred_on_trainset)
-  # y_test_log = np.log(y_test)
-  # y_pred_log = np.log(y_pred)
-  # print('training RMSE: ', np.sqrt(mean_squared_error(y_pred_on_trainset_log, y_train_log)))
-  # print('testing RMSE: ', np.sqrt(mean_squared_error(y_test_log, y_pred_log)))
+  y_pred_on_trainset = linear_regression.predict(X_train)
+  y_pred = linear_regression.predict(X_test)
+  y_pred_on_trainset[y_pred_on_trainset < target_col.min()] = target_col.min()
+  y_pred[y_pred < target_col.min()] = target_col.min()
+  y_train_log = np.log(y_train)
+  y_test_log = np.log(y_test)
+  y_pred_on_trainset_log = np.log(y_pred_on_trainset)
+  y_pred_log = np.log(y_pred)
+  print('training RMSE: ', np.sqrt(mean_squared_error(y_pred_on_trainset_log, y_train_log)))
+  print('testing RMSE: ', np.sqrt(mean_squared_error(y_test_log, y_pred_log)))
 
 
 
